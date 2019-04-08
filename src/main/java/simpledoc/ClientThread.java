0ba0e80292f4a28c8f7564285681e6efd9da1daf
Mark A Maze <mark.amaze@gmail.com>
2019-04-08 12:10:07 -0700

    implemented https

--- src/main/java/simpledoc/ClientThread.java
+++ src/main/java/simpledoc/ClientThread.java
@@ -3,83 +3,77 @@ package simpledoc;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStream;
-import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpsExchange;
 
 public class ClientThread extends Thread {
 
-	public ClientThread(HttpExchange exchange, ServiceLoader loader) {
-		System.out.println("ClientThread instance created");
-		System.out.println(exchange.getRequestURI());
-	}
-
-//COMMENTED OUT WHILE SETTING UP ON HEROKU
-	// private HttpExchange exchange;
-	// private ServiceLoader services;
-	//
-	// public ClientThread(HttpExchange exchange, ServiceLoader loader) {
-	// 	this.exchange = exchange;
-	// 	this.services = loader;
-	// 	try { this.join(); }
-	// 	catch (InterruptedException e) { e.printStackTrace(); }
-	// }
-	//
-	// @Override
-	// public void run() {
-	// 	switch(this.exchange.getRequestURI().getPath()) {
-	// 		case "/":
-	// 			loadFile("index.html");
-	// 			break;
-	// 		case "/simpledoc.bundle.js":
-	// 			loadFile("simpledoc.bundle.js");
-	// 			break;
-	// 		default:
-	// 			handleResourceRequest();
-	// 			break;
-	// 	}
-	//
-	// }
-	//
-	// private void handleResourceRequest() {
-	//
-	// 	ResourceRequest request = new ResourceRequest(
-	// 									this.exchange.getRequestMethod(),
-	// 									this.exchange.getRequestURI().getPath(),
-	// 									this.exchange.getRequestURI().getQuery(),
-	// 									this.exchange.getRequestBody());
-	//
-	// 	ResourceResponse response = this.services.load(request.module(), request.method())
-	// 										.run(request);
-	//
-	// 	try {
-	// 		this.exchange.sendResponseHeaders(  response.responseCode(),
-	// 											response.bodyLength() );
-	// 		OutputStream out = this.exchange.getResponseBody();
-	// 		out.write(response.writableBody());
-	// 		out.close();
-	// 	} catch (IOException e) { e.printStackTrace(); }
-	//
-	// }
-	//
-	// private void loadFile(String directory) {
-	// 	OutputStream out = null;
-	// 	FileReader in = null;
-	// 	String path = "/home/mark/code/portfolio/simpledoc/src/resources/dist/";
-	//
-	// 	try {
-	// 		exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "[::1]:3333");
-	// 		exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST");
-	// 		exchange.sendResponseHeaders(200, 0);
-	// 		out = exchange.getResponseBody();
-	//
-	// 		in = new FileReader(path + directory);
-	// 		int i;
- 	// 		while((i = in.read()) != -1) {
-	// 			char character = (char)i;
-	// 			out.write((byte) character);
-	// 		}
-	// 		in.close();
-	// 		out.flush();
-	// 		out.close();
-	// 	} catch (IOException e) { e.printStackTrace(); }
-	// }
+	 private HttpsExchange exchange;
+	 private ServiceLoader services;
+	
+	 public ClientThread(HttpsExchange exchange, ServiceLoader loader) {
+	 	this.exchange = exchange;
+	 	this.services = loader;
+	 	try { this.join(); }
+	 	catch (InterruptedException e) { e.printStackTrace(); }
+	 }
+	
+	 @Override
+	 public void run() {
+	 	switch(this.exchange.getRequestURI().getPath()) {
+	 		case "/":
+	 			loadFile("index.html");
+	 			break;
+	 		case "/simpledoc.bundle.js":
+	 			loadFile("simpledoc.bundle.js");
+	 			break;
+	 		default:
+	 			handleResourceRequest();
+	 			break;
+	 	}
+	
+	 }
+	
+	 private void handleResourceRequest() {
+	
+	 	ResourceRequest request = new ResourceRequest(
+	 									this.exchange.getRequestMethod(),
+	 									this.exchange.getRequestURI().getPath(),
+	 									this.exchange.getRequestURI().getQuery(),
+	 									this.exchange.getRequestBody());
+	
+	 	ResourceResponse response = this.services.load(request.module(), request.method())
+	 										.run(request);
+	
+	 	try {
+	 		this.exchange.sendResponseHeaders(  response.responseCode(),
+	 											response.bodyLength() );
+	 		OutputStream out = this.exchange.getResponseBody();
+	 		out.write(response.writableBody());
+	 		out.close();
+	 	} catch (IOException e) { e.printStackTrace(); }
+	
+	 }
+	
+	 private void loadFile(String directory) {
+	 	OutputStream out = null;
+	 	FileReader in = null;
+	 	String path = "/home/mark/code/portfolio/simpledoc/src/resources/dist/";
+	
+	 	try {
+	 		exchange.getResponseHeaders().set("Access-Control-Allow-Origin", "[::1]:3333");
+	 		exchange.getResponseHeaders().set("Access-Control-Allow-Methods", "POST");
+	 		exchange.sendResponseHeaders(200, 0);
+	 		out = exchange.getResponseBody();
+	
+	 		in = new FileReader(path + directory);
+	 		int i;
+ 	 		while((i = in.read()) != -1) {
+	 			char character = (char)i;
+	 			out.write((byte) character);
+	 		}
+	 		in.close();
+	 		out.flush();
+	 		out.close();
+	 	} catch (IOException e) { e.printStackTrace(); }
+	 }
 }
--- src/main/java/simpledoc/Simpledoc.java
+++ src/main/java/simpledoc/Simpledoc.java
@@ -1,27 +1,79 @@
 package simpledoc;
 
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.net.InetSocketAddress;
+import java.security.KeyStore;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLParameters;
 
-import com.sun.net.httpserver.HttpContext;
-import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsExchange;
+import com.sun.net.httpserver.HttpsParameters;
+import com.sun.net.httpserver.HttpsServer;
 
 public class Simpledoc {
 
-	public static void main(String[] args) {
-		
-		//TODO: question -> should service loader be static or instantiated for each request?
-		ServiceLoader loader = new ServiceLoader();
 
-		try{
-			HttpServer server = HttpServer.create(new InetSocketAddress(3333), 0);
-	        HttpContext context = server.createContext("/");
-	        context.setHandler(exchange -> {
-	        	System.out.println("client request incoming: " + exchange.getRemoteAddress());	        		
-	        	new ClientThread(exchange, loader).start();});
-	        server.start();
-	        String ipaddress = server.getAddress().toString();
-	        System.out.println("Server running at: " + ipaddress);
-	    }catch(IOException e) {e.printStackTrace();}
-	}
+    public static void main(String[] args) throws Exception {
+
+        try {
+            // initialize the HTTPS server
+        	InetSocketAddress address = new InetSocketAddress("127.0.0.1", 3333);
+            HttpsServer httpsServer = HttpsServer.create(address, 0);
+
+            // initialize the key store
+            char[] password = "password".toCharArray();
+            KeyStore key_store = KeyStore.getInstance("JKS");
+            FileInputStream file_input = new FileInputStream("/home/mark/code/portfolio/simpledoc/src/main/java/simpledoc/testkey.jks");
+            key_store.load(file_input, password);
+
+            // setup the key manager factory
+            KeyManagerFactory key_manager_factory = KeyManagerFactory.getInstance("SunX509");
+            key_manager_factory.init(key_store, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory trust_manager_factory = TrustManagerFactory.getInstance("SunX509");
+            trust_manager_factory.init(key_store);
+
+            // setup the HTTPS context and parameters
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(key_manager_factory.getKeyManagers(), trust_manager_factory.getTrustManagers(), null);
+        
+            
+            //TODO: consider refacoring
+            // I may want to create a context for each of the main module types
+            // and load the service and resource request object directly?
+            // then the server response could be send from the server directly
+            httpsServer.createContext("/", exchange -> {
+            	new ClientThread((HttpsExchange)exchange, new ServiceLoader()).run(); });
+
+            
+            httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialize the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception e) {e.printStackTrace();}
+                }
+            });           
+            httpsServer.setExecutor(null);
+            httpsServer.start();
+           
+            System.out.println("server started on port " + httpsServer.getAddress().getPort());
+
+        } catch (Exception e) {e.printStackTrace();}
+    }
+
 }
