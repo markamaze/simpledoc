6ea1fc6ce2f1c735e5fcf9a174794a2d7bb50e32
Mark A Maze <mark.amaze@gmail.com>
2019-04-27 11:05:12 -0700

    initialize testing file

---
+++ src/test/java/ResourceRequestTest.java
@@ -0,0 +1,56 @@
+package simpledoc;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class ResourceRequestTest {
+
+  @Test
+  public void storeRequestMethod() {
+
+  }
+
+  @Test
+  public void storeRequestURL() {
+
+  }
+
+  @Test
+  public void storeRequestQuery() {
+
+  }
+
+  @Test
+  public void storeRequestBody() {
+
+  }
+
+  @Test
+  public void provideRequestMethod() {
+    //method should be one of the strings: GET/POST/PUT/DELETE
+
+  }
+
+  @Test
+  public void provideRequestModule() {
+    //module should be a string matching a loaded service module
+  }
+
+  @Test
+  public void provideRequestResourcePath() {
+    //should provide a list of strings that maps to a single resource
+  }
+
+  @Test
+  public void provideRequestQuery() {
+    //should provide a list of key/value pairs as string
+  }
+
+  @Test
+  public void provideRequestBodyData() {
+    //should provide a list of objects that were contained in the data object of the request body
+    
+  }
+
+}
