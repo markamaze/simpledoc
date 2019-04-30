b70dcab1b9a97e024d83c18aae878831107cddc1
Mark A Maze <mark.amaze@gmail.com>
2019-04-30 14:45:34 -0700

    replace initial file with empty class w/ notes

--- src/test/java/ResourceRequestTest.java
+++ src/test/java/ResourceRequestTest.java
@@ -4,53 +4,19 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 
+/* Test notes:
+
+make sure that each of the request elements are accessible
+and structure as such:  method-> string
+                        url-> list of strings
+                        query-> map of key/value pairs
+                        body_data-> list of data item maps
+
+
+*/
+
+
+
 public class ResourceRequestTest {
-
-  @Test
-  public void storeRequestMethod() {
-
-  }
-
-  @Test
-  public void storeRequestURL() {
-
-  }
-
-  @Test
-  public void storeRequestQuery() {
-
-  }
-
-  @Test
-  public void storeRequestBody() {
-
-  }
-
-  @Test
-  public void provideRequestMethod() {
-    //method should be one of the strings: GET/POST/PUT/DELETE
-
-  }
-
-  @Test
-  public void provideRequestModule() {
-    //module should be a string matching a loaded service module
-  }
-
-  @Test
-  public void provideRequestResourcePath() {
-    //should provide a list of strings that maps to a single resource
-  }
-
-  @Test
-  public void provideRequestQuery() {
-    //should provide a list of key/value pairs as string
-  }
-
-  @Test
-  public void provideRequestBodyData() {
-    //should provide a list of objects that were contained in the data object of the request body
-    
-  }
 
 }
