c140acb1c8766c512eb081ddfe283afd7974e828
Mark A Maze <mark.amaze@gmail.com>
2019-05-03 01:18:19 -0700

    normalizing +cleanup

--- src/main/java/simpledoc/services/agency/AgencyFactory.java
+++ src/main/java/simpledoc/services/agency/AgencyFactory.java
@@ -13,8 +13,8 @@ public class AgencyFactory implements ModuleObjectFactory {
 	
 	public ModuleObject build(Map<String, Object> data_item) {
 		String type = (String) data_item.get("type");
-		if(type.equalsIgnoreCase("AGENCY_AGENT")) return buildAgent(data_item);
-		if(type.equalsIgnoreCase("AGENCY_DEFINITION")) return buildDefinition(data_item);
+		if(type.equalsIgnoreCase("AGENCY.AGENT")) return buildAgent(data_item);
+		if(type.equalsIgnoreCase("AGENCY.DEFINITION")) return buildDefinition(data_item);
 		if(type.equalsIgnoreCase("AGENCY.CATEGORY")) return buildCategory(data_item);
 			
 		return null;
@@ -22,7 +22,7 @@ public class AgencyFactory implements ModuleObjectFactory {
 
 	@SuppressWarnings("unchecked")
 	private ModuleObject buildCategory(Map<String, Object> data_item) {
-		AgentCategoryObject new_agent_category = null;
+		AgentCategory new_agent_category = null;
 		
 		String object_id = data_item.get("id") != null ? (String) data_item.get("id") : null;
 		String object_type = (String) data_item.get("type");
@@ -34,8 +34,8 @@ public class AgencyFactory implements ModuleObjectFactory {
 		Map<String, Object> data_definition = (Map<String, Object>) object_data.get("category_data_structure");
 
 		if(object_id.equalsIgnoreCase("new")) {
-			new_agent_category = new AgentCategoryObject(UUID.randomUUID().toString(), object_type);
-		} else new_agent_category = new AgentCategoryObject(object_id, object_type);
+			new_agent_category = new AgentCategory(UUID.randomUUID().toString(), object_type);
+		} else new_agent_category = new AgentCategory(object_id, object_type);
 		
 		new_agent_category.setCategoryLabel(label);
 		new_agent_category.setCategoryBehavior(category_type);
@@ -48,21 +48,19 @@ public class AgencyFactory implements ModuleObjectFactory {
 
 	@SuppressWarnings("unchecked")
 	private ModuleObject buildDefinition(Map<String, Object> data_item) {
-		AgentDefObject new_agent_definition = null;
+		AgentDefinition new_agent_definition = null;
 		String object_id = data_item.get("id") != null ? (String) data_item.get("id") : null;
 		String object_type = (String) data_item.get("type");
 		Map<String, Object> object_data = data_item.get("object_data") != null ? (Map<String, Object>) data_item.get("object_data") : null;
 
 		String label = object_data.get("definition_label") != null ? (String) object_data.get("definition_label") : null;
 		String category_id = object_data.get("category_id") != null ? (String) object_data.get("category_id") : null;
-		String security_setting = object_data.get("definition_security") != null ? (String) 
-object_data.get("definition_security") : null;
-		Map<String, Object> data_definition = object_data.get("definition_data_structure") != null ? (Map<String, Object>) 
-object_data.get("definition_data_structure") : null;
+		String security_setting = object_data.get("definition_security") != null ? (String) object_data.get("definition_security") : null;
+		Map<String, Object> data_definition = object_data.get("definition_data_structure") != null ? (Map<String, Object>) object_data.get("definition_data_structure") : null;
 
 		if(object_id == null) {
-			new_agent_definition = new AgentDefObject(UUID.randomUUID().toString(), object_type);
-		} else new_agent_definition = new AgentDefObject(object_id, object_type);
+			new_agent_definition = new AgentDefinition(UUID.randomUUID().toString(), object_type);
+		} else new_agent_definition = new AgentDefinition(object_id, object_type);
 		
 		new_agent_definition.setDefinitionLabel(label);
 		new_agent_definition.setCategoryId(category_id);
@@ -73,7 +71,7 @@ object_data.get("definition_data_structure") : null;
 
 	@SuppressWarnings("unchecked")
 	private ModuleObject buildAgent(Map<String, Object> data_item) {
-		AgencyObject new_object;
+		AgentObject new_object;
 
 		String object_id = data_item.get("id") != null ? (String) data_item.get("id") : null;
 		String object_type = (String) data_item.get("type");
@@ -84,14 +82,14 @@ object_data.get("definition_data_structure") : null;
 		Map<String, Object> agent_data = (Map<String, Object>) object_data.get("agent_data");
 		Map<String, Object> data_definition = (Map<String, Object>) object_data.get("agent_data_structure");
 	
-		String agent_type = object_type + "_" + 
-							(String) data_definition.get("category_label") + "_" +
+		String agent_type = object_type + "." + 
+							(String) data_definition.get("category_label") + "." +
 							(String) data_definition.get("definition_label");
 				
 				
 		if(object_id == null || object_id.equalsIgnoreCase("uuid")) {
-			new_object = new AgencyObject(UUID.randomUUID().toString(), agent_type);
-		} else new_object = new AgencyObject(object_id, agent_type);
+			new_object = new AgentObject(UUID.randomUUID().toString(), agent_type);
+		} else new_object = new AgentObject(object_id, agent_type);
 		
 		if(agent_link_id == null || agent_link_id.equalsIgnoreCase("uuid")) {
 			new_object.setAgentLinkId(UUID.randomUUID().toString());
