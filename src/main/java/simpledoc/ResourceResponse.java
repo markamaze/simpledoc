04c5deae21c07f67618d42c1dbb0febda9de96ea
Mark A Maze <mark.amaze@gmail.com>
2019-05-07 16:22:32 -0700

    added code to empty methods and a little reorganization

--- src/main/java/simpledoc/ResourceResponse.java
+++ src/main/java/simpledoc/ResourceResponse.java
@@ -1,46 +1,55 @@
 package simpledoc;
 
-import java.util.stream.Stream;
 
+//can add handling of setting response headers in here when ready	
 public class ResourceResponse {
 
-	private boolean operation_success_flag = false;
-	private boolean db_success_flag = false;
+	
+	private String response_body = "";
+	private boolean request_success = false;
+	private boolean storage_op_success = false;
+	private boolean response_success = false;
 
-	public void setDbSuccessFlag(boolean db_operation) { this.db_success_flag = db_operation; }
-	public boolean getDbSuccessFlag() { return this.db_success_flag; }
+	
+	public boolean getStorageOpFlag() { return this.storage_op_success; }
+	public void setStorageOpFlag(boolean flag) { 
+		this.storage_op_success = flag; 
+		if(!flag) setResponseBody("Error in storage operation"); }
 
+	
+	public boolean getRequestOpFlag() { return this.request_success; }
+	public void setRequestOpFlag(boolean flag) { 
+		this.request_success  = flag; 
+		if(!flag) setResponseBody("Error in processing received request"); }
 
-	public void setResponseBody(Stream<Object> map) {
-		// TODO Auto-generated method stub
+	
+	public boolean getResponseOpFlag() { return this.response_success; }
+	public void setResponseOpFlag(boolean flag) { 
+		this.response_success = flag; 
+		if(!flag) setResponseBody("Error in building response"); }
 
+	
+	public String body() { return this.response_body; }
+	public void setResponseBody(Object body) {
+		this.response_body = ParseObject.writeJSONString(body);
 	}
 
+	
 	public int responseCode() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public long bodyLength() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public byte[] writableBody() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void setOperationSuccessFlag(boolean b) { this.operation_success_flag  = b; }
-	public boolean getOperationSuccessFlag() { return this.operation_success_flag; }
-
-	
-	
-	public void setErrorMessage(String string) {
-		// TODO Auto-generated method stub
+		/* possible states:
+		 * 		request handled successfully
+		 * 		problem with request
+		 * 		problem with storage
+		 * 		problem forming response
+		 *
+		 *	build a switch statement to handle
+		 *	parsing the flags to determine which
+		 *	code to return
+		 *
+		 *	for now, just return 200 by default
+		 * */
 		
+		
+		return 200;
 	}
-
-
-
 }
