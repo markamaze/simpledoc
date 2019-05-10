263669468720f7718752d7c67c3c5202af3a7f18
Mark A Maze <mark.amaze@gmail.com>
2019-05-10 09:44:11 -0700

    init validation handling classes

---
+++ src/main/java/simpledoc/utilities/ValidationObject.java
@@ -0,0 +1,15 @@
+package simpledoc.utilities;
+
+import java.util.Map;
+import java.util.UUID;
+
+
+
+
+
+public class ValidationObject {
+  private boolean isValid;
+  private String string_value;
+  private UUID uuid_value;
+  private Map<String, Object> map_value;
+}
