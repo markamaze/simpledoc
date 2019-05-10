f9716c8050d9c0106d6ddc5daab18edb4a18dc29
Mark A Maze <mark.amaze@gmail.com>
2019-05-10 09:52:40 -0700

    add import to relocated utility

--- src/main/java/simpledoc/ResourceRequest.java
+++ src/main/java/simpledoc/ResourceRequest.java
@@ -1,5 +1,6 @@
 package simpledoc;
 
+import simpledoc.utilities.ParseObject;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.HashMap;
--- src/main/java/simpledoc/ResourceResponse.java
+++ src/main/java/simpledoc/ResourceResponse.java
@@ -1,41 +1,42 @@
 package simpledoc;
 
+import simpledoc.utilities.ParseObject;
 
-//can add handling of setting response headers in here when ready	
+//can add handling of setting response headers in here when ready
 public class ResourceResponse {
 
-	
+
 	private String response_body = "";
 	private boolean request_success = false;
 	private boolean storage_op_success = false;
 	private boolean response_success = false;
 
-	
+
 	public boolean getStorageOpFlag() { return this.storage_op_success; }
-	public void setStorageOpFlag(boolean flag) { 
-		this.storage_op_success = flag; 
+	public void setStorageOpFlag(boolean flag) {
+		this.storage_op_success = flag;
 		if(!flag) setResponseBody("Error in storage operation"); }
 
-	
+
 	public boolean getRequestOpFlag() { return this.request_success; }
-	public void setRequestOpFlag(boolean flag) { 
-		this.request_success  = flag; 
+	public void setRequestOpFlag(boolean flag) {
+		this.request_success  = flag;
 		if(!flag) setResponseBody("Error in processing received request"); }
 
-	
+
 	public boolean getResponseOpFlag() { return this.response_success; }
-	public void setResponseOpFlag(boolean flag) { 
-		this.response_success = flag; 
+	public void setResponseOpFlag(boolean flag) {
+		this.response_success = flag;
 		if(!flag) setResponseBody("Error in building response"); }
 
-	
+
 	public String body() { return this.response_body; }
 	public void setResponseBody(Object body) {
 		String data_string = ParseObject.writeJSONString(body);
 		this.response_body = "{data:" + data_string + "}";
 	}
 
-	
+
 	public int responseCode() {
 		/* possible states:
 		 * 		request handled successfully
@@ -49,8 +50,8 @@ public class ResourceResponse {
 		 *
 		 *	for now, just return 200 by default
 		 * */
-		
-		
+
+
 		return 200;
 	}
 }
