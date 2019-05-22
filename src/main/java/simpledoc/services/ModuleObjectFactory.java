e103992d1ae76a0b393113a5b0fd45366330d149
mark <mark@localhost.localdomain>
2019-05-22 10:54:48 -0700

    class throws exception

--- src/main/java/simpledoc/services/ModuleObjectFactory.java
+++ src/main/java/simpledoc/services/ModuleObjectFactory.java
@@ -1,6 +1,9 @@
 package simpledoc.services;
 
+import simpledoc.exceptions.ServiceErrorException;
+import simpledoc.RequestData;
+
 
 public interface ModuleObjectFactory {
-	public ModuleObject build(Object data_item);
+	public ModuleObject build(RequestData data_item) throws ServiceErrorException;
 }
