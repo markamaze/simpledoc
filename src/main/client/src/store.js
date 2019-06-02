8a91c6809a099ee1e95562ee2a94f68e620b6cdf
mark <mark@localhost.localdomain>
2019-06-02 00:14:52 -0700

    connect Agency reducer

--- src/main/client/src/store.js
+++ src/main/client/src/store.js
@@ -3,12 +3,12 @@ import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 // import appStateReducer from './component/appstate_reducer'
-// import agencyReducer from './component/agency/agency_reducer'
+import agencyReducer from './modules/Agency/module_reducer'
 
 var middleware = composeWithDevTools(applyMiddleware(thunk))
 
 var reducers = combineReducers({ appState: null,
-																 agency: null
+																 agency: agencyReducer
 															  })
 
 export default createStore(reducers, middleware)
