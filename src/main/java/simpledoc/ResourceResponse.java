94d0fb908ea5424a7b3716f12db02123bcb53862
mark <mark@localhost.localdomain>
2019-06-02 00:12:16 -0700

    add quotes around json key "data" & "error"

--- src/main/java/simpledoc/ResourceResponse.java
+++ src/main/java/simpledoc/ResourceResponse.java
@@ -21,14 +21,14 @@ public class ResourceResponse {
 
 	public ResourceResponse setResponse(Set<?> body, int status_code){
 		String data_string = JSONStringer.valueToString(body);
-		this.response_body = "{data:" + data_string + "}";
+		this.response_body = "{\"data\":" + data_string + "}";
 		this.response_code = status_code;
 		return this;
 	}
 
 	public ResourceResponse setResponse(String body, int status_code){
-		if(status_code == 200) this.response_body = "{data:" + body + "}";
-		else this.response_body = "{error:" + body + "}";
+		if(status_code == 200) this.response_body = "{\"data\":" + body + "}";
+		else this.response_body = "{\"error\":" + body + "}";
 
 		this.response_code = status_code;
 
