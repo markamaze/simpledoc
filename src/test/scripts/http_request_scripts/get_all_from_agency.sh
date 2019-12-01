8cfdb1749b39e6d32a162bc1fe76ac185a3f1d8a
Mark A Maze <mark.amaze@gmail.com>
2019-12-01 12:25:31 -0800

    update code to read all data

--- src/test/scripts/http_request_scripts/get_all_from_agency.sh
+++ src/test/scripts/http_request_scripts/get_all_from_agency.sh
@@ -1,1 +1,1 @@
-curl --request GET localhost:3000/Agency/
+curl --request GET localhost:3000/Agency/agents && curl --request GET localhost:3000/Agency/agentTemplates && curl --request GET localhost:3000/Agency/structuralNodes && curl --request GET localhost:3000/Agency/users && curl --request GET localhost:3000/Agency/dataTags
