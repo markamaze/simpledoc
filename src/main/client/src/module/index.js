e40c6fda318379e2c769647dc58347a07868c2cf
Mark A Maze <mark.amaze@gmail.com>
2020-01-13 02:51:15 -0800

    export array of imported modules

---
+++ src/main/client/src/module/index.js
@@ -0,0 +1,9 @@
+import agency from './agency/index'
+import workspace from './workspace/index'
+import store from '../store'
+
+export const moduleStore = store
+
+export default module = [
+  agency
+]
