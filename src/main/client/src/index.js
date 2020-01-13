1b82eaf39bccfa1ea486f67a6165f7ddb55bad3f
Mark A Maze <mark.amaze@gmail.com>
2020-01-13 02:31:11 -0800

    import modules and inject into Layout

--- src/main/client/src/index.js
+++ src/main/client/src/index.js
@@ -3,22 +3,19 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-
 import Layout from './layout/Layout'
 import store from './store'
-import {loadAgencyStore} from './module/agency/module_actions'
+import appModules from './module/index'
 
 if (module.hot) {
   module.hot.accept()
 }
 
-loadAgencyStore()
-
 ReactDOM.render(
   (
     <Provider store={store}>
       <BrowserRouter history={history}>
-        <Layout />
+        <Layout modules={appModules} />
       </BrowserRouter>
     </Provider>
   ),document.getElementById("app")
