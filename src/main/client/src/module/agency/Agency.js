e41dd6a54fec4fc451279fbc38126d99b4bfc186
Mark A Maze <mark.amaze@gmail.com>
2020-01-17 03:09:33 -0800

    updates following refactored agencyObject prototypes

--- src/main/client/src/module/agency/Agency.js
+++ src/main/client/src/module/agency/Agency.js
@@ -1,12 +1,9 @@
 import React from 'react'
 
 import Overlay from '../../components/Overlay'
-import DataTableWrapper from '../../components/DataTableWrapper'
-import { user } from './moduleObjects/user'
-import { dataTag } from './moduleObjects/dataTag'
-import { structuralNode } from './moduleObjects/structuralNode'
-import { agentTemplate } from './moduleObjects/agentTemplate'
-import { agent } from './moduleObjects/agent'
+import { agencyObject } from './moduleObjects/agencyObject'
+import TreeDisplay from './moduleComponents/TreeDisplay'
+import List from './moduleComponents/List'
 
 /*
   purpose:
@@ -15,7 +12,8 @@ import { agent } from './moduleObjects/agent'
         -
 */
 
-export default class Agency extends React.Component {
+
+class Agency extends React.Component {
   constructor(props){
     super(props)
     this.state = {
@@ -25,110 +23,33 @@ export default class Agency extends React.Component {
     }
   }
 
-  agencyTree(id, depth){
-    if(!id) return <div className="empty-component">no node id identified</div>
-
-    let _depth = ++depth
-    let node = this.props.structuralNodes.find( node => node.id === id)
-
-    let children = node.getChildren(node.id, this.props.structuralNodes)
-
-    return  <div className="tree-node" style={{paddingLeft: `${depth/2}rem`}} key={`tree-node-${node.id}`}>
-              <div className={`tree-node-header${this.state.activeComponent && this.state.activeComponent.id === node.id ? "-active" : ""}`} >
-
-                <span className="tree-node-header-title" onClick={()=> this.openInOverlay(node, "card")} >
-                  { node.structuralNode_label }
-                </span>
-                <span className="tree-node-header-action" onClick={() => this.openInOverlay(node, "builder")} >
-                  build
-                </span>
-                <span className="tree-node-header-action" onClick={() => this.openInOverlay(node, "editor")}>
-                  edit
-                </span>
-
-              </div>
-
-              <div className="tree-node-children" style={{paddingLeft: `${depth/2 + 0.25}rem`}}>
-                { children.map( child => this.agencyTree(child.id, _depth) )}
-              </div>
-
-            </div>
-  }
-  objectList(type){
-    let columns, data, title
-
-    switch(type){
-      case "agentTemplate":
-        title = "Agent Templates"
-        data = this.props.agentTemplates
-        columns = [{ name: "Templates", selector: "agentTemplate_label"}]
-        break
-      case "dataTag":
-        title = "Agency Tags"
-        data = this.props.dataTags
-        columns = [{ name: "Tags", selector: "dataTag_label"}]
-        break
-      case "user":
-        title = "Users"
-        data = this.props.users
-        columns = [{ name: "Users", selector: "username"}]
-        break
-    }
-
-
-    const RowComponent = ({data}) =>{
-      let displayComponent = data.display({card: true, ...this.props})
-      return  <div>
-                <div onClick={() => this.openInOverlay(data, "builder")}>open builder</div>
-                { displayComponent }
-              </div>}
-
-    return  <DataTableWrapper
-                className="object-list"
-                title={title}
-                actions={[<div onClick={() => this.newObject(type)}>+</div>]}
-                fixedHeader noTableHead overflowY expandableRows
-                expandableRowsComponent={<RowComponent />}
-                columns={columns}
-                data={data} />
-  }
-
-
-  newObject(type){
-
-    let newObject
-
-    if(type === "user") newObject = user({id: "new_object"})
-    else if(type === "dataTag") newObject = dataTag({id: "new_object"})
-    else if(type === "agentTemplate") newObject = agentTemplate({id: "new_object"})
-    else if(type === "structuralNode") newObject = structuralNode({id: "new_object"})
-    else if(type === "agent") newObject = agent({id: "new_object"})
-    else throw "invalid object type"
+  newChildNode(parentNode){
+    let newNode = agencyObject("structuralNode", {id: "new_object", structuralNode_parent_id: parentNode.id}, this.handleError.bind(this))
 
     this.setState({
-      activeComponent: newObject.display({builder: true, ...this.props}),
+      activeComponent: newNode.display({builder: true, ...this.props}, this.handleError.bind(this)),
       renderOverlay: true
     })
   }
+
   openInOverlay(data, displayType){
     this.setState({
-      activeComponent: data.display({[`${displayType}`]: true, ...this.props}),
+      activeComponent: data.display({[`${displayType}`]: true, ...this.props}, this.handleError.bind(this)),
       renderOverlay: true
     })
   }
 
+  createObject(type){
+    let obj = agencyObject(type, {id: "new_object"}, this.handleError.bind(this))
+    this.setState({
+      activeComponent: obj.display({builder: true, ...this.props}, this.handleError.bind(this)),
+      renderOverlay: true
+    })
+  }
+
+  handleError(error){}
 
   render() {
-    const activeView = (type, rootId) => {
-      switch(type){
-        case "agency": return this.agencyTree(rootId, 0)
-        case "agent": return this.chainOfCommand(rootId)
-        case "agentTemplate":
-        case "dataTag":
-        case "user": return this.objectList(type)
-      }
-    }
-
     return  <div className="module">
 
               <div className="module-toolbar">
@@ -142,16 +63,102 @@ export default class Agency extends React.Component {
                       onClick={() => this.setState({activeView: "dataTag"})}>Tags</div>
               </div>
 
-              <div className="module-viewport">{ activeView(this.state.activeView, this.props.rootNodeId) }</div>
+              <div className="module-viewport">
 
+              {
+                this.state.activeView !== "user" ? null :
+                  <List
+                      dataSet={this.props.users}
+                      columns={[
+                        {label: "Username", selector: "username"},
+                        {label: "Password", selector: "password"},
+                        {label: "userId", selector: "id"}
+                      ]}
+                      itemComponent={ item => item.display(this.props, error => {throw new Error("error displaying AgencyObject")})}
+                      itemActions={[]}
+                      headerComponent={<div>Agency Users</div>}
+                      footerComponent={<div>footer</div>} />
+                  // <List
+                  //     title={"Users"}
+                  //     columns={[{ name: "Users", selector: "username"}]}
+                  //     data={this.props.users}
+                  //     headerActionCreators={[
+                  //       <div onClick={() => this.createObject("user")}>+</div>
+                  //     ]}
+                  //     rowActionCreators={[]}
+                  //     onError={error => this.handleError(error)} />
+              }
+              </div>
               {
                 !this.state.renderOverlay ? null :
                   <Overlay
                       header=""
                       content={this.state.activeComponent}
-                      closeOverlay={() => this.setState({renderOverlay: false, activeComponent: null})}/>
+                      closeOverlay={() => this.setState({renderOverlay: false, activeComponent: null})}
+                      onError={error => this.handleError(error)} />
               }
 
             </div>
-  }
+    }
 }
+
+// Agency.defaultProps = {
+//   rootNode: agencyObject("structuralNode", {
+//               id: "root",
+//               structuralNode_parent_id: "root",
+//               structuralNode_label: "Default Loaded"},
+//               error => console.log(error))
+// }
+// Agency.propTypes = {
+//   rootNode: {}
+// }
+
+export default Agency
+
+
+// {
+//   this.state.activeView !== "agency"? null :
+//     !this.props.rootNode ?
+//         <button onClick={() => this.createObject("structuralNode")} className="empty-component">
+//             Build Agency
+//         </button>
+//     :   <TreeDisplay
+//             rootItem={this.props.rootNode}
+//             getChildren={item => item.typeFunctions.getChildren.call(item, this.props) }
+//             itemClickAction={item => this.openInOverlay(item, "card")}
+//             itemButtons={[
+//               { onClick: item => this.openInOverlay(item, "builder"), label: "build"},
+//               { onClick: item => this.openInOverlay(item, "editor"), label: "edit" }]} />
+// }
+// {
+//   this.state.activeView !== "agent" ? null :
+//     <TreeDisplay
+//         rootItem={this.props.rootNode.getSupervisor.call(this.props.rootNode, this.props)}
+//         getChildren={agent => {/*not sure yet*/}}
+//         headerClickAction={agent => this.openInOverlay(agent, "card")}
+//         headerButtons={[]} />
+// }
+// {
+//   this.state.activeView !== "agentTemplate" ? null :
+//     <List
+//         title={"Agent Templates"}
+//         columns={[{ name: "Templates", selector: "agentTemplate_label"}]}
+//         data={this.props.agentTemplates}
+//         headerActionCreators={[
+//           <div onClick={() => this.createObject("agentTemplate")}>+</div>
+//         ]}
+//         rowActionCreators={[]}
+//         onError={error => this.handleError(error)} />
+// }
+// {
+//   this.state.activeView !== "dataTag" ? null :
+//     <List
+//         title={"Agency Tags"}
+//         columns={[{ name: "Tags", selector: "dataTag_label"}]}
+//         data={this.props.dataTags}
+//         headerActionCreators={[
+//           <div onClick={() => this.createObject("dataTag")}>+</div>
+//         ]}
+//         rowActionCreators={[]}
+//         onError={error => this.handleError(error)} />
+// }
