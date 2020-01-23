6067d3eed06465f195e7396ba7b942172ea031a1
Mark A Maze <mark.amaze@gmail.com>
2020-01-22 16:07:11 -0800

    refactored props passing, added style wrappers

--- src/main/client/src/module/agency/moduleComponents/AgencyObject.js
+++ src/main/client/src/module/agency/moduleComponents/AgencyObject.js
@@ -5,57 +5,66 @@ import Properties from './Properties'
 import DataTags from './DataTags'
 import Assignments from './Assignments'
 import Roles from './Roles'
-
+import { CardWrapper, EditorWrapper, BuilderWrapper } from '../agencyStyles'
 import ErrorBoundary from '../agencyUtils/ErrorBoundary'
 
 
-function Card(props){
-  const [dataItem, setDataItem] = useState(props.dataItem)
-  const propValue = ()=> ({...props, ...{dataItem: dataItem, setDataItem: setDataItem}})
 
-  const cardTools = displayProps => ({
-    objectData: displayProps.objectData ? <ObjectData.ShowValues {...propValue} /> : null,
-    properties: displayProps.properties ? <Properties.ShowValues {...propValue} /> : null,
-    tags: displayProps.tags ? <DataTags.ShowTags {...propValue} /> : null,
-    assignments: displayProps.assignments ? <Assignments.Links {...propValue} /> : null,
-    roles: displayProps.roles ? <Roles.Links {...propValue} /> : null
+function Card(props){
+  const propsValue = { dataItem: props.dataItem, ...props.displayProps }
+
+  const cardTools = setProps => ({
+    objectData: props.displayProps.objectData ? <ObjectData.ShowValues {...setProps} /> : null,
+    properties: props.displayProps.properties ? <Properties.ShowValues {...setProps} /> : null,
+    tags: props.displayProps.tags ? <DataTags.ShowTags {...setProps} /> : null,
+    assignments: props.displayProps.assignments ? <Assignments.Links {...setProps} /> : null,
+    roles: props.displayProps.roles ? <Roles.Links {...setProps} /> : null
   })
-  let tools = Object.values(cardTools(props.displayProps)).filter( tool => tool !== null )
-  return <div><div>in Card</div>{ tools }</div>
+
+
+  let tools = Object.values(cardTools(propsValue)).filter( tool => tool !== null )
+  try{
+    return  <CardWrapper><div>in Card</div>{tools}</CardWrapper>
+  } catch(error){ props.onError(new Error(`${error}: error rendering AgencyObject Card`))}
 }
 
 
 function Editor(props){
   const [dataItem, setDataItem] = useState(props.dataItem)
-  const propValue = ()=> ({...props, ...{dataItem: dataItem, setDataItem: setDataItem}})
+  const propsValue = {dataItem: dataItem, setDataItem: setDataItem, ...props.displayProps}
 
-  const editorTools = displayProps => ({
-      objectData: displayProps.objectData ? <ObjectData.ShowValues fullSet {...propValue} /> : null,
-      properties: displayProps.properties ? <Properties.ModifyValues {...propValue} /> : null,
-      tags: displayProps.tags ? <DataTags.ShowTags {...propValue} /> : null,
-      assignments: displayProps.assignments ? <Assignments.ImplementAssignment {...propValue} /> : null,
-      roles: displayProps.roles ? <Roles.CreateRole {...propValue} /> : null
+  const editorTools = setProps => ({
+      objectData: props.displayProps.objectData ? <ObjectData.ShowValues {...setProps} /> : null,
+      properties: props.displayProps.properties ? <Properties.ModifyValues {...setProps} /> : null,
+      tags: props.displayProps.tags ? <DataTags.ShowTags {...setProps} /> : null,
+      assignments: props.displayProps.assignments ? <Assignments.ImplementAssignment {...setProps} /> : null,
+      roles: props.displayProps.roles ? <Roles.CreateRole {...setProps} /> : null
     })
-  let tools = Object.values(editorTools(props.displayProps)).filter( tool => tool !== null )
 
-  return  <div className="agencyObject-editor"><div>in editor</div>{ tools }</div>
+  let tools = Object.values(editorTools(propsValue)).filter( tool => tool !== null )
+
+  try {
+  return  <EditorWrapper><div>in editor</div>{ tools }</EditorWrapper>
+  } catch(error){ props.onError(new Error(`${error}: error rendering AgencyObject Editor`))}
 }
 
 
 function Builder(props){
   const [dataItem, setDataItem] = useState(props.dataItem)
-  const propValue = ()=> ({...props, ...{dataItem: dataItem, setDataItem: setDataItem}})
+  const propsValue = {dataItem: dataItem, setDataItem: setDataItem, ...props.displayProps}
 
-  const builderTools = displayProps => ({
-    objectData: displayProps.objectData ? <ObjectData.Modify {...propValue} /> : null,
-    properties: displayProps.properties ? <Properties.CreateProperties {...propValue} /> : null,
-    tags: displayProps.tags ? <DataTags.SelectTags {...propValue} /> : null,
-    assignments: displayProps.assignments ? <Assignments.CreateAssignment {...propValue} /> : null,
-    roles: displayProps.roles ? <Roles.CreateRole {...propValue} /> : null
+  const builderTools = setProps => ({
+    objectData: props.displayProps.objectData ? <ObjectData.Modify {...setProps} /> : null,
+    properties: props.displayProps.properties ? <Properties.CreateProperties {...setProps} /> : null,
+    tags: props.displayProps.tags ? <DataTags.SelectTags {...setProps} /> : null,
+    assignments: props.displayProps.assignments ? <Assignments.CreateAssignment {...setProps} /> : null,
+    roles: props.displayProps.roles ? <Roles.CreateRole {...setProps} /> : null
   })
-  let tools = Object.values(builderTools(props.displayProps)).filter( tool => tool !== null )
+  let tools = Object.values(builderTools(propsValue)).filter( tool => tool !== null )
 
-  return  <div className="agencyObject-builder"><div>in builder</div>{ tools }</div>
+  try{
+    return  <BuilderWrapper><div>in builder</div>{ tools }</BuilderWrapper>
+  } catch(error){ props.onError(new Error(`${error}: error rendering AgencyObject Builder`))}
 }
 
 export default { Builder, Editor, Card }
