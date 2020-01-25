1f84874dd06eccd7eb38cd0bc4a05630ebbbb101
Mark A Maze <mark.amaze@gmail.com>
2020-01-25 10:10:21 -0800

    update List styles

--- src/main/client/src/components/styles.js
+++ src/main/client/src/components/styles.js
@@ -7,77 +7,141 @@ export const ListWrapper = styled.div`
   display: block;
 
   .list-header {
-    text-align: center;
-    background: ${colors.two};
-    color: ${colors.one};
+    padding: 1rem 3rem;
+    align-self: center;
+    font-size: larger;
+    font-weight: bolder;
+    font-variant-caps: small-caps;
   }
-  .list{
+  .list-header-container{
     display: flex;
-    flex-direction: column;
-  }
-
-  .list-body{
-    background: lightgray;
-    font-size: small;
-  }
-
-  .list-row{
-    display: flex;
-    border-bottom: thin solid gray
-  }
-
-  .list-head-row{
-    background: gray;
-  }
-
-  .list-head-item {
-    font-weight: bold;
-    font-style: italic;
-    flex-grow: 1;
-  }
-
-  .list-cell{
-    display: flex;
-    flex-grow: 1;
-    justify-content: left;
-    width: ${props => 100/props.columnCount}%;
-  }
-
-  .action-cell{
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .list-row-action {
-    font-size: small;
-    padding: 0 .3rem;
-  }
-
-  .action-cell :hover{
-    font-size: normal;
-    color: blue;
-  }
-
-  .list-row-component-wrapper{
-    display: flex;
-    flex-direction: column;
-    max-height: 100%;
-    min-height: 100%;
-    max-width: 100%;
-    min-width: 100%;
-    overflow: auto;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-  .list-row-component-header{
-    display: flex;
-    background: lightgray;
-  }
-  .list-row-component-header .action-cell {
     flex-direction: row;
+    /* background: ${colors.three};
+    color: ${colors.four}; */
+    justify-content: space-between;
   }
-  .list-row-component {
-    background: white;
-    box-sizing: border-box;
+  .list-footer{}
+  .list-actions{
+
+    display: flex;
+  }
+
+  .list-overlay{
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 1rem;
+    background: rgb(0,0,0,.8);
+    color: lightgray;
+    height: 100%;
+    width: 100%;
+  }
+    .list-overlay-close{}
+    .list-overlay-body{}
+    .list-overlay-options{
+    }
+
+
+  .table{
+    display: flex;
+    flex-direction: column;
+  }
+    .table-header{
+      padding: 1rem 0 .5rem;
+      border-bottom: solid ${colors.three};
+    }
+    .table-header-column{
+      font-weight: bolder;
+      font-style: italic;
+
+    }
+    .table-body{
+      display: flex;
+      flex-direction: column;
+    }
+    .table-row{
+      border-bottom: solid thin ${colors.three};
+      line-height: 1.2rem
+    }
+    .table-row-active{
+      background: ${colors.three};
+      color: ${colors.four};
+      padding: 1rem 0;
+      margin: .5rem 0;
+    }
+
+  .row{
+    display: flex;
+  }
+    null.row-cell{
+      display: flex;
+      flex-grow: 1;
+      flex-basis: 0;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      font-size: small;
+      justify-content: center;
+      cursor: pointer;
+    }
+    .row-expanded{
+      display: flex;
+      flex-direction: row;
+    }
+    .row-drawer{
+      background: white;
+      color: black;
+      margin: 0 .5rem .5rem;
+    padding: .5rem;
+    }
+      .row-drawer-tabs{
+        display: flex;
+        justify-content: space-around;
+      }
+        .row-drawer-tab{}
+      .row-drawer-component{}
+
+  .icons{}
+  .icon{
+    display: flex;
+    flex-direction: row;
+    cursor: pointer;
+    background: ${colors.one};
+    padding: .25rem;
+  }
+
+  .tree{
+    display: flex;
+    max-width: 100%;
+    flex-wrap: wrap;
+    flex-direction: column;
+  }
+    .tree-node{
+      display: flex;
+      flex-direction: row;
+      padding: .5rem;
+      background: white;
+    }
+    .tree-branch{
+      display: flex;
+      flex-direction: row;
+      flex-wrap: wrap;
+      justify-content: space-around;
+      padding: .25rem;
+      width: 100%;
+      box-sizing: border-box;
+      border: solid thin ${colors.four};
+    }
+
+
+
+
+
+  .action-handler{
+    font-size: x-small;
+    font-style: italic;
+    padding: .2rem;
+    display: flex;
+    align-self: center;
   }
 `
