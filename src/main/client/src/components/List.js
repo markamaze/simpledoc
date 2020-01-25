5b67d866f9f0947987f87ad7e775904a4a632e2e
Mark A Maze <mark.amaze@gmail.com>
2020-01-25 10:10:05 -0800

    rebuilt List

--- src/main/client/src/components/List.js
+++ src/main/client/src/components/List.js
@@ -2,197 +2,140 @@ import React from 'react'
 import { ListWrapper } from './styles'
 
 
-/*
-  todo:
-      handle different options for selecting a row/cell
-      finish treeRows
-      build iconSet
-      apply constraint on itemComponent display -> overlay or below row
 
-*/
+export default function List(props){
+  const [showOverlay, setOverlay] = React.useState(false)
+  const [drawerComponent, setDrawerComponent] = React.useState(false)
+  const [selectedItems, setSelectedItems] = React.useState([])
 
+  const updateSelectedItems = item => {
+    isItemSelected(item) ? setSelectedItems(selectedItems.filter(selectedItem => selectedItem !== item)) : setSelectedItems([...selectedItems, item])
 
-function List(props){
-
-  const [buildAsTree, setBuildAsTree] = React.useState(checkType())
-  const [activeItem, setActiveItem] = React.useState(null)
-  const [overlayItem, setOverlayItem] = React.useState(null)
-
-  function checkType(){
-  	if(!props.heirarchyKey) return false
-
-    else if(props.dataSet.reduce((flag, item) => {
-      if(!flag) return false
-      else if(props.heirarchyKey in item) return true
-    })) return true
-
-    else return false
   }
+  const isItemSelected = item => selectedItems.find( selectedItem => selectedItem === item )
 
-
-
-  function columnTitleRow(){
-    let columnTitleRow = props.columns.map( column => column.label )
-
-    props.itemActions && props.itemActions.length > 0 ? columnTitleRow.push("") : null
-
-    return  <div className="list-head-row list-row">
-              { columnTitleRow.map(columnTitle =>
-                  <div className="list-head-item list-cell">{columnTitle}</div>) }
-            </div>
-  }
-  function addRow(dataItem){
-    return  <div className="list-row" >
-              { props.columns.map(column =>
-                  <div className="list-cell" onClick={() => setActiveItem(dataItem)}>
-                    {dataItem[column.selector]}
-                  </div>) }
-
-              { props.itemActions && props.itemActions.length > 0 ? addActionCell(dataItem) : null }
-            </div>
-  }
-  function addActionCell(dataItem){
-    return  <div className="list-cell action-cell">
-              {
-                props.itemActions.map(itemAction =>
-                  <div className="list-row-action" onClick={() => setOverlayItem(itemAction.action(dataItem))}>
-                    {itemAction.label}
-                  </div>)
-              }
-            </div>
-  }
-  function renderItemComponent(dataItem){
-    return !props.itemComponent ? null :
-      React.cloneElement(
-        props.itemComponent(dataItem),
-        [{className: `list-row-component-wrapper`, displayName: "ListItemComponent"}],
-        [ <div className="list-row-component-header">
-            <div className="list-row-component-close"
-                  onClick={() => setActiveItem(null)}>X</div>  //where'd it go?
-            { addActionCell(dataItem) }
-          </div>,
-          <div className={`${props.itemComponent(dataItem).props.className} list-row-component`}>
-            {props.itemComponent(dataItem).props.children}
-          </div>
-        ])
-  }
-  function getColumnCount(){
-    return props.itemActions && props.itemActions.length > 0 ?
-      props.columns.length + 1 :
-      props.columns.length
-  }
-
-
-
-
-  function iconSet(){}
-
-
-function activeItemAction(item){
-  if(props.onItemClick) return props.onItemClick(item)
-
-  else return renderItemComponent(item)
-}
-
-
-  function listRows(){
-    return  <div className="list-body">
-              {
-                props.dataSet.map( dataItem =>
-                  <div className="list-row-wrapper" key={`list-row-${props.key}-${dataItem.id}`}>
-                    { addRow(dataItem) }
-                    { dataItem === activeItem ? activeItemAction(dataItem) : null }
-                  </div>)
-                }
-              </div>
-  }
-
-
-
-  function treeRows(root, depth){
-    let childrenSet = props.dataSet.filter( dataItem =>
-            dataItem[props.treeNodeKey] === root[props.rootIdKey]
-            && dataItem !== root )
-
-    let _depth = ++depth
-
-    return childrenSet && childrenSet.length > 0 ?
-
-      <div className={`tree-body`} key={`tree-body-${root.id}`} >
-        <div className="tree-row-wrapper" key={`tree-row-wrapper-${root.id}`}>
-          <div className="tree-root" key={`tree-root-${root.id}`} >
-            { addRow(root) }
-            { root === activeItem ? renderItemComponent(root) : null }
-            <div className="tree-root-children-list"
-                  style={{paddingLeft: `${_depth}rem`}}>
-              { childrenSet.map( dataItem => treeRows(dataItem, _depth)) }
-            </div>
-
-          </div>
-        </div>
+  const addRow = (item, loadingFn) =>
+    <div className={`table-row${isItemSelected(item) ? "-active" : ""}`}>
+      <div className="row">
+        {
+          props.columns[props.limited ? "limited" : "expanded"].map( column =>
+            <div className="row-cell" onClick={()=> updateSelectedItems(item)}>{item[column.selector]}</div>)
+        }
       </div>
-
-      : <div className="tree-body">
-          <div className="tree-row-wrapper">
-            <div className="tree-leaf">{ addRow(root) }</div>
-            { root === activeItem ? renderItemComponent(root) : null }
+      {
+        isItemSelected(item) ? //load set of action creators that will set overlay with a component
+          <div className="row-expanded">
+            {
+              props.overlayComponents.length > 0 ? <div className="list-overlay-options">{loadOverlayHandlers(item)}</div> : null
+            }
           </div>
-        </div>
+          : null
+      }
+      {
+        isItemSelected(item) ? //load drawer as tabbed pages inside drawer when item is selectedItem
+          props.drawerComponents.length > 0 ? <div className="row-drawer">{loadingFn(item)}</div> : null
+          : null
+      }
+    </div>
+
+  const addIcon = item =>
+    <div className="icon" onClick={() => updateSelectedItems(item)}>
+      { props.iconComponent(item) }
+      {
+        isItemSelected(item) ?
+          props.overlayComponents.length > 0 ? <div className="list-overlay-options">{loadOverlayHandlers(item)}</div> : null
+          : null
+      }
+      {
+        isItemSelected(item) ?
+          props.drawerComponents.length > 0 ? <div className="row-drawer">{loadTableDrawer(item)}</div> : null
+          : null
+      }
+    </div>
+
+  const loadOverlayHandlers = item => props.overlayComponents.map( overlayComponent =>
+    <div className="action-handler" onClick={() => setOverlay(overlayComponent.component(item))}>{overlayComponent.label}</div>)
+
+  const loadTableDrawer = (item) => {
+    if(props.drawerComponents.length === 1) return props.drawerComponents[0].component(item)
+    if(props.drawerComponents.length < 1) return null
+
+    return  <div className="row-drawer">
+              <div className="row-drawer-tabs">
+                { props.drawerComponents.map( component =>
+                    <div className="row-drawer-tab" onClick={() => setDrawerComponent(component.component(item))}>{component.label}</div>) }
+              </div>
+              { drawerComponent ? <div className="row-drawer-component">{ drawerComponent}</div> : null }
+            </div>
   }
 
+// props.nodeBranch(item).map(child => buildTree(child))
+  const buildTree = (node, depth) =>
+    <div className="tree-node">
+      {props.iconComponent ? addIcon(node) : addRow(node, (item) => loadTableDrawer(item) )}
+      {props.nodeBranch(node).length > 0 ? <div className="tree-branch">{props.nodeBranch(node, ++depth).map( child => buildTree(child))}</div> : null}
+    </div>
 
-  function buildList(){
-      if(props.treeRoot && props.treeNodeKey)
-        return  <div className="tree-body">
-                  { columnTitleRow() }
-                  { treeRows(props.treeRoot, 0) }
-                </div>
+  const buildList = () =>
+    props.iconComponent ?
+      <div className="icons">{ props.tableData.map(item=>addIcon(item))}</div>
+      : <div className="table">
+          <div className="table-header row">
+            { props.columns[props.limited ? "limited" : "expanded"].map( column =>
+                <div className="table-header-column row-cell">{column.label}</div>) }
+          </div>
+          <div className="table-body">
+            { props.tableData.map( item => addRow(item, loadTableDrawer)) }
+          </div>
+        </div>
 
-      else if(props.icon && props.iconComponent)
-        return  <div className="icon-set-body">
-                  { iconSet() }
-                </div>
 
-      else
-        return  <div className="list-body">
-                  { columnTitleRow() }
-                  { listRows() }
-                </div>
-    }
+  const loadOverlay = () =>
+    <div className="list-overlay">
+      <div className="list-overlay-close" onClick={() => setOverlay(false)} >x</div>
+      <div className="list-overlay-body">{ showOverlay }</div>
+    </div>
 
-  try{
-    return  <ListWrapper columnCount={getColumnCount()} style={props.style}>
-              { !props.headerComponent ? null :
-                  <header onClick={() => setActiveItem(null)}>{props.headerComponent}</header>
-              }
-              { buildList() }
-              { overlayItem ?
-                  <div style={{position: "absolute", background: "rgb(0,0,0,.5)", color:"lightgray",top:"0",width:"100%",height:"100%"}}>
-                    <div onClick={()=> setOverlayItem(null)}>X</div>
-                    { overlayItem }
-                  </div>
+  const loadListActions = () =>
+    <div className="list-actions">
+      {
+        props.listActions.map(item =>
+          <div className="action-handler" onClick={() => item.action()}>{item.label}</div> )
+      }
+    </div>
 
-              : null }
+
+  try {
+    return  <ListWrapper className="list" style={props.style}>
+              <div className="list-header-container">
+                { props.headerComponent ? <div className="list-header">{props.headerComponent}</div> : null }
+                { props.listActions && props.listActions.length > 0 ? loadListActions() : null }
+              </div>
+              { props.tableData ? buildList() : null }
+              { props.root && props.nodeBranch ? <div className="tree">{buildTree(props.root, 0)}</div> : null }
+              { props.footerComponent ? <div className="list-footer">{props.footerComponent}</div> : null }
+              { showOverlay ? loadOverlay() : null }
             </ListWrapper>
-  }catch(error){ throw error}
+  } catch(error) {
+    console.trace(error)
+    throw new Error(`${error}: Error building List`)}
 }
 
+/*List.propTypes = {
+  root: {},
+  nodeBranch: ()=>{} ,
 
-// List.displayName = "List"
-// List.propTypes = {
-//   dataSet: undefined, //an array of objects
-//   columns: undefined, //array of objects defining column display properties
-//   treeRoot: undefined
-//   treeNodeKey: undefined, //a key in each item of dataSet for building tree, if unset, will display as a list
-//   headerComponent: undefined, //optional header element
-//   footerComponent: undefined, //optional footer element
-//   onClickAction: undefined, //function acting on the row element
-//   itemActions: undefined //action handlers next to each item
-//
-//
-// }
-// List.defaultProps = {
-//
-// }
+  columns: {
+    limited: [],
+    expanded: []
+  },
+  limited: boolean,
+  iconComponent: {},
+  tableData: [],
 
-export default List
+  listActions: [],
+  drawerComponents: [],
+  overlayComponents: [],
+  headerComponent: {},
+  footerComponent: {}
+}*/
