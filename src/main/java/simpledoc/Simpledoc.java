b7208ab4f56e54f4d8f0f8999855495bcb2dc612
Mark A Maze <mark.amaze@gmail.com>
2020-02-06 10:26:21 -0800

    change storage/loader to static singletons and inject into clientthread

--- src/main/java/simpledoc/Simpledoc.java
+++ src/main/java/simpledoc/Simpledoc.java
@@ -2,28 +2,35 @@ package simpledoc;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.SQLException;
 
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpServer;
 
 public class Simpledoc {
+	static StorageControl storage;
+	static ServiceLoader loader;
 
 	public static void main(String[] args) {
-
-		ServiceLoader loader = new ServiceLoader();
+		String database_url = System.getenv("JDBC_DATABASE_URL");
 		Integer port = Integer.parseInt(System.getenv("PORT"));
-//		Integer port = 3000;
+
 
 		try{
+		  storage = new StorageControl(database_url);
+		  loader = new ServiceLoader();
+
 		  HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-	    HttpContext context = server.createContext("/");
-	    context.setHandler(exchange -> {
-	      System.out.println("client request incoming: " + exchange.getRemoteAddress());
-	      new ClientThread(exchange, loader).start();
-      });
-	    server.start();
-	    String ipaddress = server.getAddress().toString();
-	    System.out.println("Server running at: " + ipaddress);
-	  }catch(IOException e) {e.printStackTrace();}
+		  HttpContext context = server.createContext("/");
+		  context.setHandler(exchange -> {
+			  System.out.println("client request incoming: " + exchange.getRequestMethod() + exchange.getRequestURI());
+			  new ClientThread(exchange, storage, loader).start();
+		  });
+
+		  server.start();
+		  System.out.println("Server running at: " + server.getAddress().toString());
+		}catch(IOException e) {e.printStackTrace();} catch (SQLException e) {
+			e.printStackTrace();
+	  }
 	}
 }
