4da8b078e510a8ab0c251e6f05b5c32edd3ce88c
Mark A Maze <mark.amaze@gmail.com>
2020-02-06 10:15:21 -0800

    initialize method to validate completion rules

--- src/main/java/simpledoc/services/forms/FormsValidator.java
+++ src/main/java/simpledoc/services/forms/FormsValidator.java
@@ -1,6 +1,7 @@
 package simpledoc.services.forms;
 
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import simpledoc.exceptions.UnsupportedServiceRequest;
@@ -12,6 +13,11 @@ public class FormsValidator extends ModuleValidation{
 	@Override
 	public boolean validateRequest(Set<ModuleObjectData> data, String method, List<String> resource)
 			throws UnsupportedServiceRequest {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public static boolean validateCompletionRule(Entry<String, Object> entry) {
 		// TODO Auto-generated method stub
 		return true;
 	}
