d9a6c60983450e30704800207e86a4d8b818f2d8
Mark A Maze <mark.amaze@gmail.com>
2020-02-06 10:07:25 -0800

    reworked loading of module service functions

--- src/main/java/simpledoc/ServiceLoader.java
+++ src/main/java/simpledoc/ServiceLoader.java
@@ -1,32 +1,26 @@
 package simpledoc;
 
-import simpledoc.exceptions.UnsupportedServiceRequest;
-import java.util.HashMap;
-import java.util.Map;
-
+import simpledoc.exceptions.ServiceErrorException;
+import java.util.List;
 import simpledoc.services.agency.AgencyService;
 import simpledoc.services.forms.FormsService;
 
 
-//TODO: rework this with routes - handle unknown requests
 public class ServiceLoader {
-	private Map<String, Map<String, ServiceFunction>> services;
-
+	private static AgencyService agency;
+	private static FormsService forms;
+	
 	public ServiceLoader() {
-		services = new HashMap<String, Map<String, ServiceFunction>>();
-		services.put("Agency", new AgencyService<>().provideServices());
-		services.put("Forms", new FormsService<>().provideServices());
+		agency = new AgencyService();
+		forms = new FormsService();
 	}
 
+	public ServiceFunction load(List<String> resource_path, String method) throws ServiceErrorException {
+		String module = resource_path.get(0);
 
-
-	public ServiceFunction load(String module, String method ) throws UnsupportedServiceRequest {
-		Map<String, ServiceFunction> service_module = services.get(module);
-		if (service_module.equals(null)) throw new UnsupportedServiceRequest("service requested not found");
-
-		ServiceFunction service = service_module.get(method);
-		if (service.equals(null)) throw new UnsupportedServiceRequest("service does not support this HTTPMethod");
-
-		return service;
+		
+		if(module.equals("Agency")) return agency.moduleRoutes(resource_path);
+		else if(module.equals("Forms")) return forms.moduleRoutes(resource_path);
+		else throw new ServiceErrorException("Requested module not found");
 	}
 }
