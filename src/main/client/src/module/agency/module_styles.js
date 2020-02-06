1d91c163ed4efb759bdad78f282c0362bd20ff4f
Mark A Maze <mark.amaze@gmail.com>
2020-02-06 09:52:40 -0800

    consolidated style sheets

--- src/main/client/src/module/agency/module_styles.js
+++ src/main/client/src/module/agency/module_styles.js
@@ -43,3 +43,28 @@ export const DisplayWrapper = styled(ModuleStyles)`
     border-radius: .2rem;
   }
 `
+
+
+export const AgencyPageWrapper = styled.div`
+  width: 100%;
+  max-width: 100%;
+  height: 100%;
+  max-height: 100%;
+  overflow: auto;
+  .dataTag {
+    display: flex;
+    font-size: x-small;
+    background: ${colors.two};
+    color: ${colors.one};
+    padding: .2rem;
+    margin: .2rem;
+    border-radius: .5rem;
+    justify-content: center;
+  }
+`
+
+export const CardWrapper = styled.div``
+
+export const EditorWrapper = styled.div``
+
+export const BuilderWrapper = styled.div``
