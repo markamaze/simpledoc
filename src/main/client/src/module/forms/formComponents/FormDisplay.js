2f32b12f84a070f1eeffaae789e1a402047fb99c
Mark A Maze <mark.amaze@gmail.com>
2020-02-06 10:00:57 -0800

    reworked implementation of form components

--- src/main/client/src/module/forms/formComponents/FormDisplay.js
+++ src/main/client/src/module/forms/formComponents/FormDisplay.js
@@ -1,28 +1,30 @@
 import React from 'react'
 
 import { DocumentWrapper, EditorWrapper, CreatorWrapper } from '../styles'
-import * as FormComponents from './FormComponents'
+import * as Form from './Form'
+import * as Section from './Section'
+import * as Layout from './Layout'
+import * as Element from './Element'
+import * as CompletionRules from './CompletionRules'
+import * as SecuritySettings from './SecuritySettings'
+
 
 function Document(props){
   const propsValue = { dataItem: props.dataItem, ...props.displayProps }
 
-  const cardTools = setProps => ({
-    formInfo: setProps.formInfo ? <FormComponents.Form {...setProps} /> : null,
-
-
-
-
-    sections: setProps.sections ? <Sections.Display {...setProps} /> : null,
-    layouts: setProps.layouts ? <Layouts.Display {...setProps} /> : null,
-    elements: setProps.elements ? <Elements.ShowValues {...setProps} /> : null,
+  const documentTools = setProps => ({
+    formInfo: setProps.formInfo ? <Form.Display {...setProps} /> : null,
+    sections: setProps.sections ? <Section.Display {...setProps} /> : null,
+    layouts: setProps.layouts ? <Layout.Display {...setProps} /> : null,
+    elements: setProps.elements ? <Element.ShowValues {...setProps} /> : null,
     completionRules: setProps.completionRules ? <CompletionRules.ShowValues {...setProps} /> : null,
-    securityRules: props.securityRules ? <Security.Enforce {...setProps} /> : null
+    securityRules: props.securityRules ? <SecuritySettings.Enforce {...setProps} /> : null
   })
 
-  let tools = Object.values(cardTools(propsValue)).filter( tool !== null )
+  let tools = Object.values(documentTools(propsValue)).filter( tool => tool !== null )
 
   try{
-    return <DocumentWrapper>{tools}</DocumentWrapper>
+    return <DocumentWrapper>in document{tools}</DocumentWrapper>
   } catch(err) {throw err}
 }
 
@@ -31,24 +33,19 @@ function Editor(props){
   const propsValue = { form: props.form, ...props.displayProps }
 
   const editorTools = setProps => ({
-//TODO: write component tools to display FormObject as Editor
-// show form as: a document to be filled out and submitted
-// display each formObject based on security_settings/display_settings
-// assign to formSets
-
-    formInfo: setProps.formInfo ? <Info.ShowValues {...setProps} /> : null,
-    sections: setProps.sections ? <Sections.Display {...setProps} /> : null,
-    layouts: setProps.layouts ? <Layouts.Display {...setProps} /> : null,
-    elements: setProps.elements ? <Elements.Inputs {...setProps} /> : null,
+    formInfo: setProps.formInfo ? <Form.Display {...setProps} /> : null,
+    sections: setProps.sections ? <Section.Display {...setProps} /> : null,
+    layouts: setProps.layouts ? <Layout.Display {...setProps} /> : null,
+    elements: setProps.elements ? <Element.EditValues {...setProps} /> : null,
     completionRules: setProps.completionRules ? <CompletionRules.ShowValues {...setProps} /> : null,
     securityRules: props.securityRules ? <Security.Enforce {...setProps} /> : null
 
   })
 
-  let tools = Object.values(editorTools(propsValue)).filter( tool !== null )
+  let tools = Object.values(editorTools(propsValue)).filter( tool => tool !== null )
 
   try{
-    return <EditorWrapper>{tools}</EditorWrapper>
+    return <EditorWrapper>in editor{tools}</EditorWrapper>
   }catch(err){throw err}
 }
 
@@ -56,23 +53,23 @@ function Editor(props){
 function Creator(props){
   const propsValue = { form: props.form, ...props.displayProps }
 
-  const builderTools = setProps => ({
+  const creatorTools = setProps => ({
 //TODO: write component tools to display FormObject as builder
 // show form as: builder mode -> add/modify/delete the sections, layouts, and element definitions
 // set properties for completion_rules and security_settings/display_settings
 
-    formInfo: setProps.formInfo ? <Info.SetValues {...setProps} /> : null,
-    sections: setProps.sections ? <Sections.Modify {...setProps} /> : null,
-    layouts: setProps.layouts ? <Layouts.Modify {...setProps} /> : null,
-    elements: setProps.elements ? <Elements.Define {...setProps} /> : null,
+    formInfo: setProps.formInfo ? <Form.Define {...setProps} /> : null,
+    sections: setProps.sections ? <Section.Define {...setProps} /> : null,
+    layouts: setProps.layouts ? <Layout.Define {...setProps} /> : null,
+    elements: setProps.elements ? <Element.Define {...setProps} /> : null,
     completionRules: setProps.completionRules ? <CompletionRules.Define {...setProps} /> : null,
     securityRules: props.securityRules ? <Security.Define {...setProps} /> : null
   })
 
-  let tools = Object.values(builderTools(propsValue)).filter( tool !== null )
+  let tools = Object.values(creatorTools(propsValue)).filter( tool => tool !== null )
 
   try{
-    return <CreatorWrapper>{tools}</CreatorWrapper>
+    return <CreatorWrapper>in creator{tools}</CreatorWrapper>
   }catch(err){throw err}
 }
 
